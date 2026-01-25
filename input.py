a41853a4668f0d301441ee28406a54ac40b39ff1
alexj <alexjvd@outlook.com>
2026-01-24 22:07:30 -0500

    Added Edge Cases, need to test all cases

--- input.py
+++ input.py
@@ -22,7 +22,22 @@ def create_priority_map(f, length):
     priority_map = {}
     for i in range(length):
         str_line = f.readline().strip()
-        priority_map[i + 1] = {"preferences": list(map(int, str_line.split())), "pair": None}
+        if str_line != '':
+            try:
+                priority_list = list(map(int, str_line.split()))
+            except ValueError:
+                raise ValueError("ERROR: Non-numeric ID in priority list for selector " + str(i + 1))
+            priority_list_len = len(priority_list)
+            if priority_list_len != length:
+                raise ValueError("ERROR: Priority list length of selector " + str(
+                    i + 1) + " does match the amount of options available.")
+            elif len(set(map(int, str_line.split()))) != priority_list_len:
+                raise ValueError("ERROR: Priority list elements of selector " + str(i + 1) + " are non-unique.")
+            elif not all(0 <= p <= length for p in priority_list):
+                raise ValueError("ERROR: Invalid selector ID in priority list for selector " + str(i + 1))
+            priority_map[i + 1] = {"preferences": priority_list, "pair": None}
+        else:
+            raise ValueError("ERROR: Not enough input records.")
     return priority_map
 
 
@@ -31,6 +46,13 @@ filename = input("Some filenames in the codebase are input.txt, empty.txt, one-o
 
 with open(filename, "r") as f:
     line = f.readline()
-    size = int(line)
-    hospital_map = create_priority_map(f, size)
-    student_map = create_priority_map(f, size)
+    if line != '':
+        size = int(line)
+        print("\nLoading hospital priorities ...")
+        hospital_map = create_priority_map(f, size)
+        print("Loading student priorities ...\n")
+        student_map = create_priority_map(f, size)
+        if f.readline() != '':
+            raise ValueError("ERROR: Too many input records.")
+    else:
+        raise ValueError("ERROR: Empty input file.")
--- main.py
+++ main.py
@@ -11,23 +11,27 @@ import input
 hospitals = input.hospital_map
 students = input.student_map
 
-for h_id, hospital in hospitals.items():
-    if hospital["pair"] is None:
-        for student in hospital["preferences"]:
-            if students[student]["pair"] is None:
-                hospital["pair"] = student
-                students[student]["pair"] = h_id
-                break
-            elif students[student]["preferences"].index(students[student]["pair"]) > students[student][
-                "preferences"].index(h_id):
-                hospitals[students[student]["pair"]] = None
-                hospital["pair"] = student
-                students[student]["pair"] = h_id
-                break
+if hospitals and students:
+    print("Gale Shapely Algorithm initiating ...")
+    for h_id, hospital in hospitals.items():
+        if hospital["pair"] is None:
+            for student in hospital["preferences"]:
+                if students[student]["pair"] is None:
+                    hospital["pair"] = student
+                    students[student]["pair"] = h_id
+                    break
+                elif students[student]["preferences"].index(students[student]["pair"]) > students[student][
+                    "preferences"].index(h_id):
+                    hospitals[students[student]["pair"]] = None
+                    hospital["pair"] = student
+                    students[student]["pair"] = h_id
+                    break
 
-# Output Handler: Algorithm results are loaded to output.txt
-with open("output.txt", "w") as f:
-    for hospital in hospitals:
-        f.write(str(hospital) + " " + str(hospitals[hospital]["pair"]) + "\n")
+    print("Gale Shapely Algorithm successfully implemented!\n")
 
-print("Matching results located in output.txt")
+    # Output Handler: Algorithm results are loaded to output.txt
+    with open("output.txt", "w") as f:
+        for hospital in hospitals:
+            f.write(str(hospital) + " " + str(hospitals[hospital]["pair"]) + "\n")
+
+    print("Matching results located in output.txt")
